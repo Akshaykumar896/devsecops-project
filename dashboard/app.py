99e98d3ad87196f140483b85681f94a943a5cc50
Akshaykumar896 <prajapatiakshay8968@gmail.com>
2026-06-11 00:58:17 +0530

    main

---
+++ dashboard/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+
+    with open('scan-results.json') as f:
+        data = json.load(f)
+
+    return render_template(
+        'index.html',
+        data=data
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
